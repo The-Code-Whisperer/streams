359048e87aa3ad917a97a03c18513c0f19a47cc5
Steven Kim <hansomekim@gmail.com>
2020-07-13 16:58:25 -0400

    added notes.txt

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { Provider } from 'react-redux'; // Provider is a component from react redux that already comes with a store property that recognizes redux stores. This will allow us to use redux stores by using the special createStore function and storing it in a variable, and setting it to the Provider component's store property
+import { createStore, applyMiddleware, compose } from 'redux'; // don't really need the applyMiddleware and compose modules from the redux library just yet, but we're importing it because it's necessary for redux devtools(?) and we're going to have middleware eventually anyway, so might as well wire it up now.
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
